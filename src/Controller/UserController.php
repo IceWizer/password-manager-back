da63bb7447fa27f8d4c86db4bfcad7ed85d25d3f
Florian Charlot <florian.charlot53@gmail.com>
2024-04-05 11:08:56 +0200

    Initial commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): JsonResponse
+    {
+        return new JsonResponse([
+            'username' => 'jane_doe',
+            'email' => '@'
+        ]);
+    }
+}
