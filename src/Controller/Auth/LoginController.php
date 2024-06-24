1104819f5fe142daa9f80038625417a97afe9d65
Florian Charlot <florian.charlot53@gmail.com>
2024-06-24 10:13:56 +0200

    fix: log out

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -11,19 +11,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class LoginController extends AbstractController
 {
     #[Route('/api/auth/login', name: 'api_login')]
-    public function index(#[CurrentUser] ?User $user): Response
+    public function login(#[CurrentUser] ?User $user): Response
     {
-        if (null === $user) {
-            return $this->json([
-                'message' => 'test',
-            ], Response::HTTP_UNAUTHORIZED);
-        }
-
-        $token = '1234'; // somehow create an API token for $user
-
         return $this->json([
-            'user'  => $user->getId(),
-            'token' => $token,
+            'user' => $user,
         ]);
     }
 
