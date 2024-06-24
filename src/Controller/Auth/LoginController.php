c959ff4f4ced2205fb2392958f446e71a321c9e2
Florian Charlot <florian.charlot53@gmail.com>
2024-06-24 11:56:05 +0200

    Fix: sign in

--- src/Controller/Auth/LoginController.php
+++ src/Controller/Auth/LoginController.php
@@ -3,10 +3,16 @@
 namespace App\Controller\Auth;
 
 use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use StrRandom;
 use Symfony\Component\Security\Http\Attribute\CurrentUser;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class LoginController extends AbstractController
 {
@@ -23,6 +29,70 @@ class LoginController extends AbstractController
     {
         return $this->json([
             'message' => 'logout successful',
+        ]);
+    }
+
+    #[Route("/api/auth/register", name: "api_register", methods: ["POST"])]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher, EntityManagerInterface $em, MailerInterface $mailer): Response
+    {
+        $payload = $request->getPayload();
+
+        // Check the unicity of the email
+        if ($em->getRepository(User::class)->findOneBy(['email' => $payload->get('email')]) === null) {
+            return $this->json([
+                'message' => 'Email already exists',
+            ], 400);
+        }
+
+        $user = new User();
+
+        $user->setEmail($payload->get('email'));
+        // Hash the password
+        $user->setPassword($passwordHasher->hashPassword($user, $payload->get('password')));
+
+        $token = StrRandom::generateRandomString(250, 350);
+
+        while ($em->getRepository(User::class)->findOneBy(['token' => $token])) {
+            $token = StrRandom::generateRandomString(250, 350);
+        }
+
+        $user->setToken($token);
+
+        $em->persist($user);
+        $em->flush();
+
+        $email = (new Email())
+            ->from('no-reply@password-manager.icewize.fr')
+            ->to($payload->get('email'))
+            ->subject('Vérification de votre adresse email')
+            ->text('Veuillez cliquer sur ce lien pour valider votre adresse email')
+            ->html('<a href="http://localhost:5173/verify-email/' . $user->getToken() . '">Cliquez ici pour valider votre adresse email</a>');
+
+        $mailer->send($email);
+
+        return $this->json([
+            'message' => 'User registered successfully',
+        ]);
+    }
+
+    #[Route("/api/auth/verify-email/{token}", name: "api_verify_email", methods: ["POST"])]
+    public function verifyEmail(string $token, EntityManagerInterface $em): Response
+    {
+        $user = $em->getRepository(User::class)->findOneBy(['token' => $token]);
+
+        if (!$user) {
+            return $this->json([
+                'message' => 'Invalid token',
+            ], 400);
+        }
+
+        $user->setEmailVerifiedAt(new \DateTimeImmutable());
+        $user->setToken(null);
+
+        $em->flush();
+
+        return $this->json([
+            'message' => 'Email verified successfully',
         ]);
     }
 }
---
+++ src/Utils/StrRandom.php
@@ -0,0 +1,17 @@
+<?php
+
+class StrRandom
+{
+    public static function generateRandomString($minLength, $maxLength)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $randomString = '';
+        $length = rand($minLength, $maxLength);
+
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, strlen($characters) - 1)];
+        }
+
+        return $randomString;
+    }
+}
